16d35be3aeffad3b65b4c6fdee27fa7d52acffc4
LeeJiangbo <584556825@qq.com>
2018-10-23 16:16:57 +0800

    Initial Commit

---
+++ AVPlayerDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AVPlayerDemo
+//
+//  Created by Lee on 2018/10/23.
+//  Copyright © 2018 Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
